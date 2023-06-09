05ee06d55c61c24496e6b25cef1602ce64df169c
shomil03 <shomil02020@gmail.com>
2023-06-09 12:12:25 +0530

    Initial Commit

---
+++ Game_and_GamePad/Game_and_GamePadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_and_GamePadApp.swift
+//  Game_and_GamePad
+//
+//  Created by Shomil Singh on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Game_and_GamePadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
